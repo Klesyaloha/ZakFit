abe939e61b0756991e1ba85f1940e1a0aed301c1
Klesya <v.klesya@hotmail.fr>
2024-12-10 16:37:43 +0100

    Initial Commit

---
+++ ZakFit/ZakFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitApp.swift
+//  ZakFit
+//
+//  Created by Klesya on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZakFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
